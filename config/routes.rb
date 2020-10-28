a492db5dbc23c5e1d9f023c71e3ea3563df9eec0
vagrant <vagrant@localhost.localdomain>
2020-10-28 10:14:28 +0000

    [add]guest_login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   root to: 'top#index'
   get 'top/about' => 'top#about'
+  
+  post '/top/guest_sign_in', to: 'top#new_guest'
+  # ゲストログイン用ルート
+
 
   resources :cafes, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
   	resources :cafe_comments, only: [:create, :destroy]
